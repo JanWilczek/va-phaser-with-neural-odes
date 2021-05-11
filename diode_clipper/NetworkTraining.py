20656692078b4d22107fc22a626699cef5dd08ac
Jan Wilczek <janwilczek@axel.audiolabs.uni-erlangen.de>
2021-05-11 09:29:23 +0200

    [WIP] SimpleRNN pipeline.

--- diode_clipper/NetworkTraining.py
+++ diode_clipper/NetworkTraining.py
@@ -1,4 +1,5 @@
 import torch
+import math
 
 
 class NetworkTraining:
@@ -11,6 +12,7 @@ class NetworkTraining:
         self.segments_in_a_batch = -1
         self.samples_between_updates = -1
         self.initialization_length = -1
+        self.model_store_path = None
 
     def run(self):
         """Run full network training."""
@@ -19,16 +21,34 @@ class NetworkTraining:
 
     def train_epoch(self):
         segments_order = torch.randperm(self.segments_count)
+        epoch_loss = 0.0
 
         for i in range(self.minibatch_count):
-            minibatch_segment_indices = segments_order[i*segments_in_a_batch:(i+1)*self.segments_in_a_batch]
+            minibatch_segment_indices = segments_order[i*self.segments_in_a_batch:(i+1)*self.segments_in_a_batch]
             input_minibatch = self.input_data[:, minibatch_segment_indices, :]
             target_minibatch = self.target_data[:, minibatch_segment_indices, :]
 
             with torch.no_grad():
                 self.network(input_minibatch[0:self.initialization_length, :, :])
 
-            
+            subsegment_start = self.initialization_length
+            minibatch_loss = 0.0
+
+            for subsequence_id in range(self.subsegments_count):
+                output = self.network(input_minibatch[subsegment_start:subsegment_start + self.samples_between_updates, :, :])
+
+                loss = self.loss(output, target_minibatch[subsegment_start:subsegment_start + self.samples_between_updates, :, :])
+                loss.backward()
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+
+                subsegment_start += self.samples_between_updates
+                epoch_loss += loss
+
+                torch.save(self.network.state_dict(), self.model_store_path)
+                
+        return epoch_loss / (self.segment_length * self.segments_count)
+
 
     @property
     def input_data(self):
@@ -43,5 +63,13 @@ class NetworkTraining:
         return self.input_data.shape[1]
 
     @property
+    def segment_length(self):
+        return self.input_data.shape[0]
+
+    @property
+    def subsegments_count(self):    
+        return int(math.ceil((self.segment_length - self.initialization_length) / self.samples_between_updates))
+
+    @property
     def minibatch_count(self):
-        return int(torch.ceil(self.segments_count / self.segments_in_a_batch))
+        return int(math.ceil(self.segments_count / self.segments_in_a_batch))
--- diode_clipper/diode_lstm.py
+++ diode_clipper/diode_lstm.py
@@ -1,4 +1,5 @@
 """Set up an LSTM-RNN architecture, run its training and test on the diode clipper data."""
+from pathlib import Path
 import torch
 import CoreAudioML.networks as networks
 import CoreAudioML.training as training
@@ -7,28 +8,31 @@ from NetworkTraining import NetworkTraining
 
 
 def create_dataset():
-    d = dataset.DatSet(data_dir='data')
+    d = dataset.DataSet(data_dir=str(Path('diode_clipper', 'data').resolve()))
 
     d.create_subset('train', frame_len=22050)
     d.create_subset('validation')
     d.create_subset('ignore')
-    d.load_file('diodeclip', set_names=['train', 'validation', 'ignore'], splits=[0.8*0.8, 0.8*0.2, 0.2])
+    d.load_file('diodeclip', set_names=['train', 'validation', 'ignore'], splits=[0.8*0.8, 0.8*0.2, (1.0 - 0.8*0.8 - 0.8*0.2)])
 
     d.create_subset('test')
     d.load_file('diodeclip', set_names='test')
 
+    return d
+
 if __name__ == '__main__':
-    training = NetworkTraining()
+    session = NetworkTraining()
     
-    training.network = networks.SimpleRNN(unit_type="LSTM", hidden_size=8, skip=0)
-    training.optimizer = torch.optim.Adam(training.network.parameters(), lr=0.001)
-    training.loss = training.ESRLoss()
+    session.network = networks.SimpleRNN(unit_type="LSTM", hidden_size=8, skip=0)
+    session.optimizer = torch.optim.Adam(session.network.parameters(), lr=0.001)
+    session.loss = training.ESRLoss()
     
-    training.dataset = create_dataset()
+    session.dataset = create_dataset()
 
-    training.epochs = 1
-    training.segments_in_a_batch = 40
-    training.samples_between_updates = 2048
-    training.initialization_length = 1000
+    session.epochs = 1
+    session.segments_in_a_batch = 40
+    session.samples_between_updates = 2048
+    session.initialization_length = 1000
+    session.model_store_path = Path('diode_clipper', 'models', 'lstm_8.pth').resolve()
 
-    training.run()
+    session.run()
