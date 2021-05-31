73fa0baf9c345aea8280b08f3594d64821fd98bd
Jan Wilczek <janwilczek@axel.audiolabs.uni-erlangen.de>
2021-05-31 11:43:55 +0200

    Added: Storing simulation parameters and results in a JSON file.

--- diode_clipper/diode_ode_numerical.py
+++ diode_clipper/diode_ode_numerical.py
@@ -9,19 +9,23 @@ from scipy.interpolate import interp1d
 from scipy.signal import resample
 import torch
 import torchaudio
+import json
 from CoreAudioML.training import ESRLoss
 from NetworkTraining import create_dataset, get_run_name
 from models.solvers import trapezoid_rule, forward_euler
 
 
-parser = argparse.ArgumentParser(description='Run diode clipper ODE numerical solver.')
-parser.add_argument('-m', '--method-name', dest='method_name', default='forward_euler')
-parser.add_argument('-u', '--upsample-factor', dest='upsample_factor', default=1, type=int)
-parser.add_argument('-l', '--length-seconds', dest='length_seconds', default=0.0, type=float)
-parser.add_argument('-s', '--input-scaling-factor', dest='input_scaling_factor', default=1.0, type=float)
-
 SOLVERS = {'trapezoid_rule': trapezoid_rule, 
            'forward_euler': forward_euler}
+
+def argument_parser():
+    parser = argparse.ArgumentParser(description='Run diode clipper ODE numerical solver.')
+    parser.add_argument('-m', '--method-name', dest='method_name', default='forward_euler')
+    parser.add_argument('-u', '--upsample-factor', dest='upsample_factor', default=1, type=int)
+    parser.add_argument('-l', '--length-seconds', dest='length_seconds', default=0.0, type=float)
+    parser.add_argument('-s', '--input-scaling-factor', dest='input_scaling_factor', default=1.0, type=float)
+    parser.add_argument('-i', '--interpolation-order', dest='interpolation_order', default=0, type=int)
+    return parser
 
 class SimulationParameters:
     def __init__(self, args):
@@ -29,6 +33,9 @@ class SimulationParameters:
         self.upsample_factor = args.upsample_factor
         self.input_scaling_factor = args.input_scaling_factor
         self.length_seconds = args.length_seconds # the same as input if <= 0 or unspecified
+        self.interpolation_order = args.interpolation_order
+        
+        self.save_json(vars(args), 'args.json')
         
         # From "Numerical Methods for Simulation of Guitar Distortion Circuits" by Yeh et al.
         self.R = 2.2e3
@@ -38,6 +45,10 @@ class SimulationParameters:
         self.__v_in = None # placeholder
         self.sampling_rate = None # placeholder
         self.t = None # placeholder
+
+    def save_json(self, json_data, filename):
+        with open(self.run_directory / filename, 'w') as f:
+            json.dump(json_data, f, indent=4)
 
     @property
     def method_name(self):
@@ -57,25 +68,31 @@ class SimulationParameters:
     @v_in.setter
     def v_in(self, v_in):
         scaled_signal_in = v_in * self.input_scaling_factor
-        if not hasattr(self, 'length_seconds') or self.length_seconds <= 0.0:
+        if self.length_seconds <= 0.0:
             self.length_seconds = scaled_signal_in.shape[0] / self.sampling_rate
         self.t = torch.arange(0, self.length_seconds, 1 / self.sampling_rate)
 
         trimmed_scaled_signal_in = scaled_signal_in[:self.t.shape[0]]
         resampled_scaled_signal_in, resampled_t = resample(trimmed_scaled_signal_in.detach().numpy(), self.upsample_factor * trimmed_scaled_signal_in.shape[0], self.t.detach().numpy())
-        self.__v_in = resampled_scaled_signal_in
         self.resampled_t = resampled_t
 
+        if self.interpolation_order == 0:
+            self.__v_in = lambda t: resampled_scaled_signal_in[int(t * self.sampling_rate * self.upsample_factor)]
+        elif self.interpolation_order == 1:
+            interpolated = interp1d(resampled_t, resampled_scaled_signal_in)
+            self.__v_in = lambda t: torch.from_numpy(interpolated(t))
+        else:
+            raise RuntimeError("Interpolation order can be 0 or 1.")
+
 def jac_diode_equation_rhs(t, v_out, p):
-    jac = - 1 / (R * C) - 2 * i_s / (C * v_t) * torch.cosh(v_out / v_t)
+    jac = - 1 / (p.R * p.C) - 2 * p.i_s / (p.C * p.v_t) * torch.cosh(v_out / p.v_t)
     return jac[:, None] # Jacobian needs to be of 1x1 size
 
 def diode_equation_rhs(t, v_out, p):
-    v_in_value = p.v_in[int(t * p.sampling_rate * p.upsample_factor)]
-    return (v_in_value - v_out) / (p.R * p.C) - 2 * p.i_s / p.C * torch.sinh(v_out / p.v_t)
+    return (p.v_in(t) - v_out) / (p.R * p.C) - 2 * p.i_s / p.C * torch.sinh(v_out / p.v_t)
 
 def main():
-    args = parser.parse_args()
+    args = argument_parser().parse_args()
     p = SimulationParameters(args)
 
     dataset = create_dataset()
@@ -89,20 +106,20 @@ def main():
 
     if p.method_name in SOLVERS.keys():
         method = SOLVERS[p.method_name]
-        # v_in = interp1d(resampled_t, resampled_scaled_signal_in)
         y_upsampled = method(diode_equation_rhs, initial_value, torch.from_numpy(p.resampled_t), args=[p])
         y = resample(y_upsampled, y_upsampled.shape[0] // p.upsample_factor)
     else:
         print(f'Defaulting to scipy.integrate.solve_ivp/{p.method_name}.')
         t_span = (p.t[0], p.t[-1])
         # v_in = interp1d(t, trimmed_scaled_signal_in)
-        result = solve_ivp(diode_equation_rhs, t_span, initial_value, method=p.method_name, t_eval=p.resampled_t, args=p, jac=jac_diode_equation_rhs)
+        result = solve_ivp(diode_equation_rhs, t_span, initial_value, method=p.method_name, t_eval=p.resampled_t, args=[p], jac=jac_diode_equation_rhs)
         print(result.message)
         y = result.y
     
     end_time = time.time()
 
-    print(f'Finished in time {end_time - start_time:.1f} seconds.')
+    duration = end_time - start_time
+    print(f'Finished in time {duration:.1f} seconds.')
 
     v_out_result = torch.Tensor(y)
 
@@ -117,6 +134,7 @@ def main():
     loss_result = loss(v_out_result_1d, true_v_out_trimmed).item()
 
     print(f'ODESolver error: {loss_result}.')
+    p.save_json({'time [s]': int(duration), 'ESRLoss': loss_result}, 'result.json')
 
     plt.figure()
     plt.plot(p.t, true_v_out_trimmed, p.t, v_out_result_1d)
