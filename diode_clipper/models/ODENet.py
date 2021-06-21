Fixed the time vector. Removed output scaling.

diode_clipper/models/ODENet.py
@@ -80,15 +80,9 @@ class ODENetDerivative(nn.Module):
         torch.Tensor of shape the same as y
             derivative of y over time at time t
         """
-        # 1st-order interpolation
-        # t_new = torch.tile(t.unsqueeze(0), (y.shape[0], 1))
-        # input_at_t = Interp1d()(self.t, self.input, t_new)
-
         # 0th-order interpolation
-        frames_count = self.input.shape[0]
-        input_at_t = self.input[int(t / self.dt) % frames_count, :]
+        input_at_t = self.input[t, :]
 
-        # mlp_input = torch.stack((y.clone(), input_at_t, t * torch.ones_like(y)), dim=1)
         mlp_input = torch.stack((y.clone(), input_at_t), dim=1)
         output = self.densely_connected_layers(mlp_input)
         
@@ -96,10 +90,10 @@ class ODENetDerivative(nn.Module):
         # ode_eq_output = diode_equation_rhs(t, y, input_at_t)
 
         # v1 
-        # return output.squeeze_(-1)
+        return output.squeeze(-1)
 
         # v2, v3
-        return self.scaling * output.squeeze(-1)
+        # return self.scaling * output.squeeze(-1)
 
 
 class ODENet(nn.Module):
@@ -130,7 +124,7 @@ class ODENet(nn.Module):
         if self.state is None:
             self.state = torch.zeros((minibatch_size,), device=self.device)
 
-        self.update_time(sequence_length, minibatch_size)
+        self.create_time_vector(sequence_length)
 
         self.derivative_network.input = x.squeeze(2)
 
@@ -142,14 +136,10 @@ class ODENet(nn.Module):
 
         return odeint_output[:-1, :, None]
 
-    def update_time(self, sequence_length, minibatch_size):
-        if self.time is None:
-            start_time = 0.0
-        else:
-            start_time = self.time[-1] + self.dt
+    def create_time_vector(self, sequence_length):
         time_vector_length = sequence_length + 1 # the last sample is for the initial value for the next subsegment
-        self.time = torch.linspace(start_time, start_time + time_vector_length * self.dt, time_vector_length, device=self.device)
-        self.derivative_network.t = torch.tile(self.time.unsqueeze(0), (minibatch_size, 1))
+        if self.time is None or self.time.shape[0] != time_vector_length:
+            self.time = torch.arange(0, time_vector_length, device=self.device)
 
     def reset_hidden(self):
         self.true_state = None