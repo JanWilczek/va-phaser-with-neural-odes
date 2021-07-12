ad21e96abefb31f26bfe1125e12fca70e7de27c9
Jan Wilczek <janwilczek@axel.audiolabs.uni-erlangen.de>
2021-07-12 11:28:12 +0200

    Unified diode_clipper/main.py and phaser/main.py

--- diode_clipper/main.py
+++ diode_clipper/main.py
@@ -1,19 +1,19 @@
 """Set up an NN architecture, run its training and test on the diode clipper data."""
+from solvers import ForwardEuler, trapezoid_rule
+from models import StateTrajectoryNetworkFF, ODENet2, ODENetDerivative2, ResidualIntegrationNetworkRK4, BilinearBlock, ExcitationSecondsLinearInterpolation
+from NetworkTraining import NetworkTraining, get_run_name, create_dataset, save_json
+import CoreAudioML.training as training
+import CoreAudioML.networks as networks
+from torchdiffeq import odeint, odeint_adjoint
+import torchaudio
+import torch
+from pathlib import Path
+from functools import partial
+import json
+import argparse
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import argparse
-import json
-from functools import partial
-from pathlib import Path
-import torch
-import torchaudio
-from torchdiffeq import odeint, odeint_adjoint
-import CoreAudioML.networks as networks
-import CoreAudioML.training as training
-from NetworkTraining import NetworkTraining, get_run_name, create_dataset, save_json
-from models import StateTrajectoryNetworkFF, ODENet2, ODENetDerivative2, ResidualIntegrationNetworkRK4, BilinearBlock, ExcitationSecondsLinearInterpolation
-from solvers import ForwardEuler, trapezoid_rule
 
 
 def argument_parser():
@@ -22,24 +22,48 @@ def argument_parser():
     ap.add_argument('--epochs', '-eps', type=int, default=20, help='Max number of training epochs to run')
     ap.add_argument('--batch_size', '-bs', type=int, default=256, help='Training mini-batch size')
     ap.add_argument('--learn_rate', '-lr', type=float, default=1e-3, help='Initial learning rate')
-    ap.add_argument('--cyclic_lr', '-y', type=float, default=None, help='If given, uses the cyclic learning rate schedule by Smith. Given learning rate parameter is used as the base learning rate, and max learning rate is this argument''s parameter.')
-    ap.add_argument('--one_cycle_lr', '-oc', type=float, default=None, help='If given, uses the one cycle learning rate schedule. Given learning rate parameter is used as the base learning rate, and max learning rate is this argument''s parameter.')
+    ap.add_argument(
+        '--cyclic_lr',
+        '-y',
+        type=float,
+        default=None,
+        help='If given, uses the cyclic learning rate schedule by Smith. Given learning rate parameter is used as the base learning rate, and max learning rate is this argument'
+        's parameter.')
+    ap.add_argument(
+        '--one_cycle_lr',
+        '-oc',
+        type=float,
+        default=None,
+        help='If given, uses the one cycle learning rate schedule. Given learning rate parameter is used as the base learning rate, and max learning rate is this argument'
+        's parameter.')
     ap.add_argument('--init_len', '-il', type=int, default=1000,
-                  help='Number of sequence samples to process before starting weight updates')
+                    help='Number of sequence samples to process before starting weight updates')
     ap.add_argument('--up_fr', '-uf', type=int, default=2048,
-                  help='For recurrent models, number of samples to run in between updating network weights, i.e the '
-                       'default argument updates every 1000 samples')
+                    help='For recurrent models, number of samples to run in between updating network weights, i.e the '
+                    'default argument updates every 1000 samples')
     ap.add_argument('--val_chunk', '-vs', type=int, default=0, help='Number of sequence samples to process'
-                                                                               'in each chunk of validation ')
+                    'in each chunk of validation ')
     ap.add_argument('--test_chunk', '-tc', type=int, default=0, help='Number of sequence samples to process'
-                                                                               'in each chunk of test')
-    ap.add_argument('--checkpoint', '-c', type=str, default=None, help='Load a checkpoint of the given architecture with the specified name.')
-    ap.add_argument('--adjoint', '-adj', action='store_true', help='Use the adjoint sensitivity method for backpropagation.')
+                    'in each chunk of test')
+    ap.add_argument('--checkpoint', '-c', type=str, default=None,
+                    help='Load a checkpoint of the given architecture with the specified name.')
+    ap.add_argument('--adjoint', '-adj', action='store_true',
+                    help='Use the adjoint sensitivity method for backpropagation.')
     ap.add_argument('--name', '-n', type=str, default='', help='Set name for the run')
-    ap.add_argument('--weight_decay', '-wd', type=float, default=0.0, help='Weight decay argument for the Adam optimizer.')
-    ap.add_argument('--teacher_forcing', '-tf', nargs='?', const='always', default='never', choices=['always', 'never', 'bernoulli'], help='Enable ground truth initialization of the first output sample in the minibatch. \n\'always\' uses teacher forcing in each minibatch;\n\'never\' never uses teacher forcing;\n\'bernoulli\' includes teacher forcing more rarely according to the fraction epochs passed.\n(default: %(default)s)')
+    ap.add_argument('--weight_decay', '-wd', type=float, default=0.0,
+                    help='Weight decay argument for the Adam optimizer.')
+    ap.add_argument(
+        '--teacher_forcing',
+        '-tf',
+        nargs='?',
+        const='always',
+        default='never',
+        choices=[
+            'always',
+            'never',
+            'bernoulli'],
+        help='Enable ground truth initialization of the first output sample in the minibatch. \n\'always\' uses teacher forcing in each minibatch;\n\'never\' never uses teacher forcing;\n\'bernoulli\' includes teacher forcing more rarely according to the fraction epochs passed.\n(default: %(default)s)')
     return ap
-
 
 
 def get_method(args):
@@ -53,6 +77,7 @@ def get_method(args):
     else:
         return CUSTOM_SOLVERS[args.method[0]]
 
+
 def get_architecture(args, dt):
     if args.method[0] == 'LSTM':
         network = networks.SimpleRNN(unit_type="LSTM", hidden_size=8, skip=0)
@@ -65,6 +90,7 @@ def get_architecture(args, dt):
         network = ODENet2(ODENetDerivative2(ExcitationSecondsLinearInterpolation(dt)), method, dt)
     return network
 
+
 def attach_scheduler(args, session):
     if args.one_cycle_lr is not None:
         session.scheduler = torch.optim.lr_scheduler.OneCycleLR(session.optimizer,
@@ -73,15 +99,16 @@ def attach_scheduler(args, session):
                                                                 final_div_factor=20,
                                                                 epochs=(session.epochs - session.epoch),
                                                                 steps_per_epoch=session.minibatch_count,
-                                                                last_epoch=(session.epoch-1),
+                                                                last_epoch=(session.epoch - 1),
                                                                 cycle_momentum=False)
     elif args.cyclic_lr is not None:
         session.scheduler = torch.optim.lr_scheduler.CyclicLR(session.optimizer,
                                                               base_lr=args.learn_rate,
                                                               max_lr=args.cyclic_lr,
                                                               step_size_up=250,
-                                                              last_epoch=(session.epoch-1),
+                                                              last_epoch=(session.epoch - 1),
                                                               cycle_momentum=False)
+
 
 def load_checkpoint(args, session, model_directory):
     # Untested
@@ -89,13 +116,39 @@ def load_checkpoint(args, session, model_directory):
         session.run_directory = model_directory / args.checkpoint
         try:
             session.load_checkpoint(best_validation=True)
-        except:
+        except BaseException:
             print("Failed to load a best validation checkpoint. Reverting to the last checkpoint.")
             session.load_checkpoint(best_validation=False)
         if session.scheduler is None:
             # Scheduler's learning rate is not loaded but overwritten
             for param_group in session.optimizer.param_groups:
                 param_group['lr'] = args.learn_rate
+
+
+def get_device():
+    return 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+def save_args(args, session):
+    save_json(vars(args), session.run_directory / 'args.json')
+    session.writer.add_text('Command line arguments', json.dumps(vars(args)))
+
+
+def test(session, sampling_rate):
+    session.device = 'cpu'
+    test_output, test_loss = session.test()
+    print(f'Test loss: {test_loss}')
+    session.writer.add_scalar('Loss/test', test_loss, session.epochs)
+
+    test_output_path = (session.run_directory / 'test_output.wav').resolve()
+    torchaudio.save(test_output_path, test_output[None, :], sampling_rate)
+
+
+def log_memory_usage(session):
+    if torch.cuda.is_available():
+        session.writer.add_scalar('Maximum GPU memory usage [MB]',
+                                  torch.cuda.max_memory_allocated('cuda') / (2 ** 20),
+                                  session.epochs)
 
 
 def get_teacher_forcing_gate(teacher_forcing_description):
@@ -106,9 +159,7 @@ def get_teacher_forcing_gate(teacher_forcing_description):
     return description_to_gate[teacher_forcing_description]
 
 
-def main():
-    args = argument_parser().parse_args()
-
+def initialize_session(args):
     session = NetworkTraining()
     session.dataset = create_dataset(validation_frame_len=args.val_chunk, test_frame_len=args.test_chunk)
     sampling_rate = session.dataset.subsets['train'].fs
@@ -118,38 +169,49 @@ def main():
     session.initialization_length = args.init_len
     session.enable_teacher_forcing = get_teacher_forcing_gate(args.teacher_forcing)
     session.loss = training.LossWrapper({'ESR': .5, 'DC': .5}, pre_filt=[1, -0.85])
-    
-    session.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    session.network = get_architecture(args, 1/sampling_rate)
+    session.device = get_device()
+    session.network = get_architecture(args, 1 / sampling_rate)
     session.transfer_to_device()
-    session.optimizer = torch.optim.Adam(session.network.parameters(), lr=args.learn_rate, weight_decay=args.weight_decay)
+    session.optimizer = torch.optim.Adam(
+        session.network.parameters(),
+        lr=args.learn_rate,
+        weight_decay=args.weight_decay)
     attach_scheduler(args, session)
-    
+
     model_directory = Path('diode_clipper', 'runs', args.method[0].lower())
-    
+
     load_checkpoint(args, session, model_directory)
 
-    run_name = get_run_name() + args.name
-    session.run_directory =  model_directory / run_name
+    run_name = get_run_name(args.name)
+    session.run_directory = model_directory / run_name
 
-    save_json(vars(args), session.run_directory / 'args.json')
-    session.writer.add_text('Command line arguments', json.dumps(vars(args)))
+    save_args(args, session)
 
-    session.run()
+    return session, sampling_rate
 
-    session.device = 'cpu'
-    test_output, test_loss = session.test()
-    print(f'Test loss: {test_loss}')
-    session.writer.add_scalar('Loss/test', test_loss, session.epochs)
 
-    test_output_path = (session.run_directory / 'test_output.wav').resolve()
-    torchaudio.save(test_output_path, test_output[None, :], sampling_rate)
-
-    if torch.cuda.is_available():
-        session.writer.add_scalar('Maximum GPU memory usage [MB]', torch.cuda.max_memory_allocated('cuda') / (2 ** 20), session.epochs)
-
+def close_session(session):
+    log_memory_usage(session)
     session.writer.close()
+
+
+def main():
+    args = argument_parser().parse_args()
+
+    session, sampling_rate = initialize_session(args)
+
+    try:
+        session.run()
+    except KeyboardInterrupt:
+        print('Training interrupted, proceeding to test.')
+
+    try:
+        test(session)
+    except KeyboardInterrupt:
+        print('Test interrupted, quitting.')
+
+    close_session(session)
 
 
 if __name__ == '__main__':
