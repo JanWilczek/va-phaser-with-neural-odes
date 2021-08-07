16a60aff8137f538ea5f208b875eaa8e95e1cfae
Jan Wilczek <janwilczek@axel.audiolabs.uni-erlangen.de>
2021-08-07 18:31:40 +0200

    Added: Script for counting the number of models parameters.

---
+++ scripts/architectures_parameters_count.py
@@ -0,0 +1,27 @@
+from functools import partial
+import torch.nn as nn
+from torchdiffeq import odeint
+from CoreAudioML.networks import SimpleRNN
+from solvers import ForwardEuler
+from architectures import ODENet, DerivativeMLP, DerivativeMLP2, StateTrajectoryNetwork, ExcitationSecondsLinearInterpolation, ResidualIntegrationNetworkRK4, BilinearBlock
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def main():
+    dt = 1 / 44100
+    networks = [SimpleRNN(unit_type="LSTM", hidden_size=8, skip=0),
+                StateTrajectoryNetwork(training_time_step=dt),
+                ResidualIntegrationNetworkRK4(BilinearBlock(input_size=2, output_size=1, latent_size=6), dt),
+                ODENet(DerivativeMLP(ExcitationSecondsLinearInterpolation(), nn.ReLU(), excitation_size=1, output_size=1, hidden_size=100), ForwardEuler(), dt),
+                ODENet(DerivativeMLP(ExcitationSecondsLinearInterpolation(), nn.ReLU(), excitation_size=1, output_size=1, hidden_size=9), ForwardEuler(), dt),
+                ODENet(DerivativeMLP(ExcitationSecondsLinearInterpolation(), nn.ReLU(), excitation_size=1, output_size=1, hidden_size=9), partial(odeint, method='implicit_adams'), dt)]
+
+    print('Number of parameters for each model' \
+          '====================================')
+
+    for network in networks:
+        print(f'{type(network).__name__}: {count_parameters(network)}')
+
+if __name__ == '__main__':
+    main()
