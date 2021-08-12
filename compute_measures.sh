ebf39426bb228a2391a85b2bf27c78f5d1324026
Jan Wilczek <janwilczek@axel.audiolabs.uni-erlangen.de>
2021-08-12 15:36:56 +0200

    Added: Fully-automated measures calculation script.

--- compute_measures.sh
+++ compute_measures.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 set -x
 
-DIODECLIP_44KHZ_TARGET=diode_clipper/data/test/diodeclip-target.wav
-DIODECLIP_22KHZ_TARGET=diode_clipper/data/test/diodeclip22050Hz-target.wav
-DIODECLIP_48KHZ_TARGET=diode_clipper/data/test/diodeclip48000Hz-target.wav
-DIODECLIP_192KHZ_TARGET=diode_clipper/data/test/diodeclip192000Hz-target.wav
+DIODECLIP_TARGETS=(diode_clipper/data/test/diodeclip-target.wav diode_clipper/data/test/diodeclip22050Hz-target.wav diode_clipper/data/test/diodeclip48000Hz-target.wav diode_clipper/data/test/diodeclip192000Hz-target.wav)
+FILES_WITH_RESULT_FILEPATHS=(44100.txt 22050.txt 48000.txt 192000.txt)
 
-for file in `cat 44100.txt`
+for i in {0..3}
 do
-    python compute_measures.py -c $DIODECLIP_44KHZ_TARGET -e $file
-done
-
-for file in `cat 22050.txt`
-do
-    python compute_measures.py -c $DIODECLIP_22KHZ_TARGET -e $file
-done
-
-for file in `cat 48000.txt`
-do
-    python compute_measures.py -c $DIODECLIP_48KHZ_TARGET -e $file
-done
-
-for file in `cat 19200.txt`
-do
-    python compute_measures.py -c $DIODECLIP_192KHZ_TARGET -e $file
+    for file in `cat ${FILES_WITH_RESULT_FILEPATHS[i]}`
+    do
+        python compute_measures.py -c ${DIODECLIP_TARGETS[i]} -e $file
+    done
 done
