c00ebb28238ddadfa672ab59ef63bfb6b31fee52
Jan Wilczek <janekw@vp.pl>
2021-09-15 14:46:01 +0300

    Added: Audio file normalization script.

---
+++ scripts/normalize_audio_file.py
@@ -0,0 +1,33 @@
+import argparse
+from pathlib import Path
+import soundfile as sf
+import pyloudnorm as pyln
+
+
+def argument_parser():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('filepath', help='Path to the audio file')
+    ap.add_argument('--target_loudness', '-lufs', default=-12, type=float, help='Loudness in LUFS to normalize to (default: %(default)s).')
+    return ap
+
+def normalize_audio_file(filepath: Path, target_loudness: int):
+    data, rate = sf.read(filepath)
+
+    # measure the loudness first 
+    meter = pyln.Meter(rate) # create BS.1770 meter
+    loudness = meter.integrated_loudness(data)
+    loudness_normalized_audio = pyln.normalize.loudness(data, loudness, target_loudness)
+
+    normalized_filename = filepath.name[:-4] + '_normalized' + filepath.name[-4:]
+    normalized_filepath = filepath.parent / normalized_filename
+
+    sf.write(normalized_filepath, loudness_normalized_audio, rate)
+
+    print(f'Normalized {filepath.name}. Original loudness: {loudness} LUFS. New loudness: {target_loudness} LUFS. Stored in {normalized_filepath}.')
+
+def main():
+    args = argument_parser().parse_args()
+    normalize_audio_file(Path(args.filepath), args.target_loudness)
+
+if __name__ == "__main__":
+    main()
