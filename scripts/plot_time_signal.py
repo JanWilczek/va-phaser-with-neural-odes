a9a91a4507192c8b50dba9fb79b9bc5fc5091642
Jan Wilczek <janekw@vp.pl>
2021-10-11 19:07:13 +0300

    Corrected the acknowledgements.

--- scripts/plot_time_signal.py
+++ scripts/plot_time_signal.py
@@ -8,21 +8,26 @@ from common import setup_pyplot_for_latex, save_tikz
 
 
 def plot(signal, output_filepath, sampling_rate):
-    # setup_pyplot_for_latex()
+    setup_pyplot_for_latex()
+    plt.rcParams.update({'font.size': 21})
+
+    time = np.arange(0, signal.shape[0]) / sampling_rate
 
     plt.figure()
-    plt.plot(signal)
+    plt.plot(time, signal)
     plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude')
+    plt.savefig(f'{output_filepath}.png', bbox_inches='tight', dpi=200)
     plt.show()
 
 def main():
     ap = ArgumentParser()
-    ap.add_argument('filepath', help='.wav file to compute STFT from.')
+    ap.add_argument('filepath', help='.wav file to display the time signal from.')
     args = ap.parse_args()
     filepath = Path(args.filepath)
     data, fs = sf.read(filepath, always_2d=True)
     data = data[:, 0] # Take just the first channel of audio
-    output_filename = filepath.name[:-4] + '_stft'
+    output_filename = filepath.name[:-4] + '_time_signal'
     output_path = filepath.parent / output_filename
     plot(data, output_path, fs)
 
