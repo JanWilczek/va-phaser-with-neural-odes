33fccc7fde6611bf10517fd1398f8e0a571904a8
Jan Wilczek <janekw@vp.pl>
2022-04-03 22:11:13 +0200

    WIP

--- scripts/compute_nmse_sdr.py
+++ scripts/compute_nmse_sdr.py
@@ -120,6 +120,13 @@ def get_target_path(directory_path):
     return target_path
 
 
+def get_session_info(directory_path: Path):
+    args_path = directory_path / 'args.json'
+    with open(args_path, 'r') as fp:
+        args = json.load(fp)
+    return SessionInfo(args)
+
+
 def get_measures(directory_path: Path):
     target_path = get_target_path(directory_path)
     output_path = get_output_path(directory_path)
@@ -148,7 +155,48 @@ def get_measures(directory_path: Path):
     return nmse_db.item(), sdr.item()
 
 
-if __name__=='__main__':
+class SessionInfo:
+    def __init__(self, configuration: dict):
+        self.configuration = configuration
+        self.normalized_mean_squared_error = None
+        self.signal_to_distortion = None
+
+    def model_name(self):
+        return self.configuration['method'] + str(self.configuration['hidden_size'])
+
+    def test_sampling_rate(self):
+        return self.configuration['test_sampling_rate']
+
+    def dataset_name(self):
+        return self.configuration['dataset_name']
+
+
+class ModelFormatter:
+    def __init__(self, model_name, measure_name, dataset_name):
+        super().__init__()
+        self.model_name = model_name
+        self.measure_name = measure_name
+        self.dataset_name = dataset_name
+        self.sampling_rate_test_results = dict()
+
+    def add_result(self, session_info: SessionInfo):
+        if session_info.model_name() == self.model_name and \
+                session_info.dataset_name() == self.dataset_name:
+            measure_value = getattr(session_info, self.measure_name)
+            if session_info.test_sampling_rate() in self.sampling_rate_test_results.keys():
+                raise RuntimeWarning(f'More than one value of a sampling rate test '
+                                     f'for model {session_info.model_name()}')
+            self.sampling_rate_test_results[session_info.test_sampling_rate()] = \
+                f'{measure_value:.2f}'
+
+    def get_data_row(self):
+        return [self.model_name, self.sampling_rate_test_results[44100],
+                self.sampling_rate_test_results[22050],
+                self.sampling_rate_test_results[48000],
+                self.sampling_rate_test_results[192000]]
+
+
+if __name__ == '__main__':
     # for each directory in the given ones
     parser = argparse.ArgumentParser(description='Compute NMSE and SDR of given models'''
                                                  ' outputs and store in a .csv file and in '
@@ -158,19 +206,30 @@ if __name__=='__main__':
                              'directories containing the trained models.')
     args = parser.parse_args()
 
-    exporter = CompositeExporter()
-    exporter.add_exporter(CSVExporter())
-    exporter.add_exporter(LatexTableExporter())
+    exporter = LatexTableExporter()
     exporter.set_labels(['Model path', 'Normalized mean squared error [dB]',
                          'Signal-to-distortion ratio [dB]'])
+
+    diode_clipper_models = dict()
+    measure_name = 'normalized_mean_squared_error'
 
     for directory_name in args.run_directories:
         directory_path = root_repository_dir() / Path(directory_name)
         # compute NMSE and SDR between the test_output and the target
         try:
+            session_info = get_session_info(directory_path)
+            name = session_info.model_name()
+            if (not name in diode_clipper_models.keys()) and (session_info.dataset_name() == 'diodeclip'):
+                diode_clipper_models[name] = ModelFormatter(name, measure_name, 'diodeclip')
             nmse_db, sdr = get_measures(directory_path)
-            exporter.append([directory_name, nmse_db, sdr])
+            session_info.normalized_mean_squared_error = nmse_db
+            session_info.signal_to_distortion = sdr
+            diode_clipper_models[name].add_result(session_info)
+
         except FileNotFoundError:
             print(f'Skipped {directory_path}')
 
-    exporter.export(root_repository_dir() / 'nmse_sdr')
+    # Diode clipper
+    for model in diode_clipper_models.values():
+        exporter.append(model.get_data_row())
+    exporter.export(root_repository_dir() / 'diode_clipper_results_nmse')
